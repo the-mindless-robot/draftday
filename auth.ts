1fb35fd1fd9d5459a255972381ee6921dd935726
Joel Arias <joelarias@protonmail.com>
2026-06-01 12:07:51 -0700

    updated login to invite only

--- auth.ts
+++ auth.ts
@@ -7,8 +7,22 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
   callbacks: {
-    authorized({ auth }) {
+    authorized({ auth, request: { nextUrl } }) {
+      const roles = auth?.user?.roles ?? []
+      if (nextUrl.pathname.startsWith("/")) {
+        return roles.includes("user")
+      }
       return !!auth
+    },
+    async signIn({ profile }) {
+      const user = await prisma.user.findUnique({
+        where: { email: profile?.email ?? "" },
+      })
+      return !!user
+    },
+    async session({ session, user }) {
+      session.user.roles = user.roles
+      return session
     },
   },
 })
